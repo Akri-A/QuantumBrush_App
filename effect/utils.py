f0b86136ea2fa5e71519fd12a9ca5a24372fb2e7
quantumjoao <joao@mothquantum.com>
2025-06-16 10:47:59 +0200

    Updated aer to use MPS (#6)

--- effect/utils.py
+++ effect/utils.py
@@ -1,4 +1,147 @@
 import numpy as np
+from qiskit import generate_preset_pass_manager
+from qiskit.primitives import BackendEstimatorV2 as Estimator
+import os
+from qiskit_aer import AerSimulator
+import colorsys 
+from itertools import product
+import matplotlib.pyplot as plt
+
+def svd(matrix=None,U=None,S=None,Vt=None):
+    if U is not None:
+        S_matrix = np.diag(S)  # Convert singular values into a diagonal matrix
+        mat = U @ S_matrix @ Vt
+        return mat
+
+    """Compute the Ordered Singular Value Decomposition (SVD) of a matrix."""
+    U, S, Vt = np.linalg.svd(matrix, full_matrices=False)
+    sorted_indices = np.argsort(S)[::-1]  # Sort singular values in descending order
+    return U[:, sorted_indices], S[sorted_indices], Vt[sorted_indices, :]
+
+
+def points_within_radius(points, radius, border = None):
+    """
+    Given a set of points and a radius, return all points within the radius.
+    Args:
+        points (np.ndarray): Array of shape (N, 2) where N is the number of points.
+        radius (int): The radius to search within.
+        border (tuple): A tuple of (height, width)
+    Returns:
+        np.ndarray: Array of points within the radius.
+    """
+    if len(points.shape) == 1:
+        points = np.array([points])
+
+    assert radius > 0, "Radius must be positive"
+    assert isinstance(points, np.ndarray), "Points must be a numpy array"
+
+    # Precompute offsets within the radius
+    y, x = np.ogrid[-radius:radius+1, -radius:radius+1]
+    mask = x**2 + y**2 <= radius**2
+    offsets = np.stack(np.nonzero(mask), axis=-1) - radius
+    # Broadcast add offsets to all points
+    all_points = points[:, None, :] + offsets[None, :, :]
+    # Reshape and get unique points
+    result = np.unique(all_points.reshape(-1, 2), axis=0)
+
+    if border is not None:
+        result = np.clip(result, [0, 0], [border[0] - 1, border[1] - 1])
+
+    return result
+
+
+
+def points_within_lasso(points,border = None):
+    min_x = np.min(points[:,1])
+    max_x = np.max(points[:,1])+1
+    min_y = np.min(points[:,0])
+    max_y = np.max(points[:,0])+1
+
+    grid = list(product(np.arange(min_y,max_y), np.arange(min_x,max_x)))
+    # Create path from polygon
+    path = plt.Path(points)
+
+    # Test which points are inside the path
+    mask = path.contains_points(grid)
+
+    # Get the pixel coordinates that are inside
+    result = np.array(grid)[mask]
+
+    if border is not None:
+        result = np.clip(result, [0, 0], [border[0] - 1, border[1] - 1])
+
+    return result
+
+
+def run_estimatore(circuit, operators, backend=None, options = None):
+    #iqm_server_url = "https://cocos.resonance.meetiqm.com/garnet:mock"  # Replace this with the correct URL
+    #provider = IQMProvider(iqm_server_url)
+    #backend = provider.get_backend('garnet')
+    #sampler = BackendSamplerV2(backend, options={"default_shots": 1000})
+    if backend is None:
+        backend = AerSimulator(method = "matrix_product_state")
+
+    estimator = Estimator(backend=backend, options=options)
+
+    pm = generate_preset_pass_manager(backend=backend, optimization_level=2)
+
+    isa_circuit = pm.run(circuit)
+
+    if isinstance(operators,list):
+        isa_observable = [op.apply_layout(isa_circuit.layout) for op in operators]
+    else:
+        isa_observable = operators.apply_layout(isa_circuit.layout)
+
+    job = estimator.run([(isa_circuit, isa_observable)])
+
+    pub_result = job.result()[0]
+    obs = pub_result.data.evs
+    return obs
+
+
+def run_estimator(circuits, operators, backend=None, options = None):
+    '''Runs the estimator on the provided circuits and operators.
+    It can receive a single circuit or a list of circuits.
+    It can receive a single operator or a list of operators or a list of list of operators (one for each circuit).
+    '''
+    
+    #iqm_server_url = "https://cocos.resonance.meetiqm.com/garnet:mock"  # Replace this with the correct URL
+    #provider = IQMProvider(iqm_server_url)
+    #backend = provider.get_backend('garnet')
+    #sampler = BackendSamplerV2(backend, options={"default_shots": 1000})
+    if backend is None:
+        backend = AerSimulator()
+
+    estimator = Estimator(backend=backend, options=options)
+
+    pm = generate_preset_pass_manager(backend=backend, optimization_level=2)
+
+    if isinstance(circuits, list):
+        isa_circuits = [pm.run(circuit) for circuit in circuits]
+    else:
+        isa_circuits = [pm.run(circuits)]
+
+    n_circuits = len(isa_circuits)
+
+    if isinstance(operators,list):
+        if isinstance(operators[0], list):
+            assert len(operators) == n_circuits, "Number of circuits and operators must match"
+            isa_observables = [[op.apply_layout(isa_circuits[i].layout) for op in ops] for i,ops in enumerate(operators)]
+        else:
+            isa_observables = [[op.apply_layout(isa_circuits[0].layout) for op in operators]] * n_circuits
+    else:
+        isa_observables = [[operators.apply_layout(isa_circuits[0].layout)]] * n_circuits
+
+    isa_inputs = list(zip(isa_circuits, isa_observables))
+    job = estimator.run(isa_inputs)
+
+    pub_result = job.result()
+    obs = [pub_result[i].data.evs for i in range(n_circuits)]
+
+    if n_circuits == 1:
+        return obs[0]
+    
+    return obs
 
 def bresenham_line(x1, y1, x2, y2):
     points = []
